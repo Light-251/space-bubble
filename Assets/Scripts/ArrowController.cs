afa19ae0aca732e0f08d083a4b9d9d796966cb03
SLP <silviolepera@gmail.com>
2024-09-07 13:41:35 +0200

    Completed Arrow control

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -7,19 +7,47 @@ public class ArrowController : MonoBehaviour
 {
 
     [SerializeField]
-    public float rotationSpeed = 100f;
+    public float rotationSpeed = 250f;
+    [SerializeField]
+    public GameObject bubblePrefab;
+    public Transform firePoint;
+    public Transform arrowTransform;
+    public float maxRotation = 155.901f;
+    public float minRotation = 29.628f;
+    public float bubbleSpeed = 15f;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Disabilita la collisione tra tutti gli oggetti con lo stesso tag "Bubble"
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Bubble"), LayerMask.NameToLayer("Bubble"));
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
-        Console.WriteLine(rotation);
-        transform.Rotate(0, 0, -rotation);
+        // Controllo della direzione della Freccia
+        float rotationZ = arrowTransform.eulerAngles.z;
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        if ((rotationZ > maxRotation && horizontalInput > 0) || (rotationZ < minRotation && horizontalInput < 0))
+        {
+            float rotation = horizontalInput * rotationSpeed * Time.deltaTime;
+            transform.Rotate(0, 0, -rotation);
+        }
+
+        // Sparo della Palla
+        // TODO Aggiungere delay tra gli spari
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shootBubble();
+        }
+    }
+    void shootBubble()
+    {
+        GameObject bubble = Instantiate(bubblePrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bubble.GetComponent<Rigidbody2D>();
+        rb.velocity = firePoint.right * bubbleSpeed;
     }
 }
